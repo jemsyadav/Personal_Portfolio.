f516e167f6b9b72e03a55c6b6a5ebcbdd93dafc1
Jems Yadav <99952368+jemsyadav@users.noreply.github.com>
2023-09-05 18:10:31 +0530

    latest code

--- developer-portfolio/src/pages/Main/Main.js
+++ developer-portfolio/src/pages/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { Helmet } from 'react-helmet'
 
 import { Navbar, Footer, Landing, About, Skills, Testimonials, Blog, Education, Experience, Contacts, Projects, Services, Achievement } from '../../components'
 import { headerData } from '../../data/headerData'
